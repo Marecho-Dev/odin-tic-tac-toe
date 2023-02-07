e07c26b81f990ed39691758ff9dc4949c77adae9
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-02-06 23:49:17 -0500

    added some grid css as well as updated the gameboard module to create 9 divs within the ttt-grid

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //gameboard as module since it only needs to be run once
 //gameboard will be an array
 
-const gameboard = (()=>{
-    let board = ["","","","","","","","",""];
+const gameboard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-
-
+  const gridContainer = document.querySelector(".ttt-grid");
+  board.forEach((box) => {
+    const div = document.createElement("div");
+    div.innerText = box;
+    console.log(div.value);
+    gridContainer.appendChild(div);
+  });
 })();
