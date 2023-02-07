211bad6e3b2adff929d10a2721aab7468a07f842
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-02-07 00:42:11 -0500

    added updateTile method as well as event listeners to each of the game board tile that update the innerText with X; git push

--- script.js
+++ script.js
@@ -6,12 +6,27 @@ const gameboard = (() => {
 
   const gridContainer = document.querySelector(".ttt-grid");
   let count = 0;
+
   board.forEach((box) => {
     const div = document.createElement("div");
-    div.setAttribute("id", count);
+    div.setAttribute("id", "box-" + count);
     div.innerText = box;
     console.log(div.value);
     gridContainer.appendChild(div);
     count++;
   });
+  const updateTile = (index, character) => {
+    const tile = document.querySelector("#" + index);
+    tile.innerText = character;
+    board[index.substring(4, 5)] = character;
+  };
+
+  const tileListener = document.querySelectorAll(".ttt-grid>div");
+  tileListener.forEach((tile) => {
+    tile.addEventListener("click", () => {
+      updateTile(tile.id, "X");
+    });
+  });
 })();
+
+
