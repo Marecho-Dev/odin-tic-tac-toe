08258a946bb62d0d894aae72c87684528e6874b8
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-02-08 00:21:51 -0500

    added winCheck function which tests to see if any of the winning game boards are there. Added conditional to append div when someone wins.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//gameboard as module since it only needs to be run once
-//gameboard will be an array
 const player = (name) => {
   let sign = "";
   if (name != "CPU") {
@@ -14,7 +12,17 @@ const player = (name) => {
 
 const gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
-
+  const winningBoard = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
+  
   const gridContainer = document.querySelector(".ttt-grid");
   let count = 0;
 
@@ -26,7 +34,15 @@ const gameboard = (() => {
     gridContainer.appendChild(div);
     count++;
   });
-  
+  const winCheck = ()=> {
+    let status = false;
+    winningBoard.forEach((layout) => {
+        if (board[layout[0]] == board[layout[1]] && board[layout[0]] == board[layout[2]] &&  board[layout[1]] != "" && board[layout[2]] != ""){
+            status = true;
+        }
+    });
+    return status;
+  };
   const updateTile = (index, character) => {
     const tile = document.querySelector("#" + index);
     tile.innerText = character;
@@ -37,7 +53,7 @@ const gameboard = (() => {
     return board[index.substring(4, 5)] == "";
   };
 
-  return { updateTile, tileFree };
+  return { updateTile, tileFree , winCheck};
 })();
 
 const game = (() => {
@@ -48,7 +64,6 @@ const game = (() => {
   const tileListener = document.querySelectorAll(".ttt-grid>div");
   tileListener.forEach((tile) => {
     tile.addEventListener("click", () => {
-      console.log(gameboard.tileFree(tile.id));
       if (gameboard.tileFree(tile.id)) {
         if (count % 2 == 0) {
           activeSign = player1.getSign();
@@ -56,6 +71,12 @@ const game = (() => {
           activeSign = player2.getSign();
         }
         gameboard.updateTile(tile.id, activeSign);
+        if(gameboard.winCheck()){
+            const div = document.createElement("div");
+            div.innerText = "winner";
+            div.setAttribute("id", "winner");
+            document.body.appendChild(div);
+        };
         count++;
       }
     });
