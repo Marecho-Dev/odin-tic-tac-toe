1f92444f711dd494a61aa93447ffbd4e5fcedfa3
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-02-07 22:50:52 -0500

    added tileFree and updateTile methods to gameboard which will get called via the game module.

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //gameboard as module since it only needs to be run once
 //gameboard will be an array
+const player = (name) => {
+  let sign = "";
+  if (name != "CPU") {
+    sign = "X";
+  } else {
+    sign = "O";
+  }
+  const getName = () => this.name;
+  const getSign = () => sign;
+  return { name, sign, getName, getSign };
+};
 
 const gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
@@ -15,18 +26,38 @@ const gameboard = (() => {
     gridContainer.appendChild(div);
     count++;
   });
+  
   const updateTile = (index, character) => {
     const tile = document.querySelector("#" + index);
     tile.innerText = character;
     board[index.substring(4, 5)] = character;
   };
 
+  const tileFree = (index) => {
+    return board[index.substring(4, 5)] == "";
+  };
+
+  return { updateTile, tileFree };
+})();
+
+const game = (() => {
+  const player1 = player("Marek");
+  const player2 = player("CPU");
+  let activeSign = player1.getSign();
+  let count = 0;
   const tileListener = document.querySelectorAll(".ttt-grid>div");
   tileListener.forEach((tile) => {
     tile.addEventListener("click", () => {
-      updateTile(tile.id, "X");
+      console.log(gameboard.tileFree(tile.id));
+      if (gameboard.tileFree(tile.id)) {
+        if (count % 2 == 0) {
+          activeSign = player1.getSign();
+        } else {
+          activeSign = player2.getSign();
+        }
+        gameboard.updateTile(tile.id, activeSign);
+        count++;
+      }
     });
   });
 })();
-
-
